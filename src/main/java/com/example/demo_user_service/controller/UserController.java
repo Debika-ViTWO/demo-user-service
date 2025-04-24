bb7bd7f180508be94c8d58b992c9b55baa1107f0
Debika-ViTWO <dbasu@vitwo.in>
2025-04-24 11:16:08 +0530

    Initial commit with GitHub Actions workflow

---
+++ src/main/java/com/example/demo_user_service/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.demo_user_service.controller;
+
+import com.example.demo_user_service.model.User;
+import com.example.demo_user_service.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
+
